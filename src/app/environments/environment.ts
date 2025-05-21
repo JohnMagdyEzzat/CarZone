e28ebe082050f53c3a258be01530ec4caafceb86
John Magdy <j.magdy18@yahoo.com>
2025-05-21 13:28:25 +0300

    remove env files

--- src/app/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://sunny-macaque-arguably.ngrok-free.app/api/',
-  defaultHeaders: {
-    'ngrok-skip-browser-warning': '69420',
-  },
-};
