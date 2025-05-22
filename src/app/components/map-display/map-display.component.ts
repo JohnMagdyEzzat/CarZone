d3489dcc25fbf1d1a811e30928335f1e89d4cc95
John Magdy <j.magdy18@yahoo.com>
2025-05-22 20:03:28 +0300

    update

--- src/app/components/map-display/map-display.component.ts
+++ src/app/components/map-display/map-display.component.ts
@@ -4,10 +4,13 @@ import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map-display',
-  imports: [CommonModule,GoogleMapsModule],
+  imports: [CommonModule, GoogleMapsModule],
   templateUrl: './map-display.component.html',
-  styleUrl: './map-display.component.css'
+  styleUrl: './map-display.component.css',
 })
 export class MapDisplayComponent {
-  zoom = 5;
+  options: google.maps.MapOptions = {
+    center: { lat: 30, lng: 31 },
+    zoom: 5,
+  };
 }
