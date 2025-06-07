a0a2772e343f4cdd05a2177591d961fb0f620450
John Magdy <j.magdy18@yahoo.com>
2025-06-07 18:39:08 +0300

    update forget password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+  FormGroup,
+  AbstractControl,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
@@ -8,13 +14,20 @@ import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { tap } from 'rxjs';
 import { LoginService } from '../../services/login.service';
 import { AuthService } from '../../services/auth.service';
-import { UserLogin } from '../../models/user.model';
+import {
+  UserForgotPassword,
+  UserLogin,
+  UserResetPassword,
+} from '../../models/user.model';
+import { AuthValidators } from '../../validators/auth.validators';
+import { LoginValidators } from '../../validators/login.validators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [ReactiveFormsModule, FontAwesomeModule, RouterLink],
+  imports: [ReactiveFormsModule, FontAwesomeModule, RouterLink, CommonModule],
 })
 export class LoginComponent implements OnInit {
   private fb = inject(FormBuilder);
@@ -23,14 +36,29 @@ export class LoginComponent implements OnInit {
   private authService = inject(AuthService);
 
   loginForm = this.fb.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+  restoreAccountForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
+  resetPasswordForm = this.fb.group({
+    otp: ['', [Validators.required, Validators.pattern(/^[0-9]{6}$/)]],
+    password: ['', [Validators.required]],
+    confirmPassword: [
+      '',
+      [Validators.required, LoginValidators.passwordMatchValidator()],
+    ],
   });
 
   faGoogle = faGoogle;
   faApple = faApple;
   faEnvelope = faEnvelope;
   faLock = faLock;
+
+  forgotBtnTitle = 'Forgot Password';
+  resetPasswordMode = false;
+  restoreAccountMode = false;
 
   constructor() {}
 
@@ -59,5 +87,49 @@ export class LoginComponent implements OnInit {
       email: this.loginForm.value.email || '',
       password: this.loginForm.value.password || '',
     };
+  }
+
+  toggleFormView(mode: string) {
+    this.currentForm().touched = true;
+    if (mode === 'reset') {
+      this.forgotBtnTitle = 'Restore Account';
+      this.resetPasswordMode = false;
+      this.restoreAccountMode = true;
+    }
+    if (mode === 'forget' && this.currentForm().valid) {
+      this.loginService
+        .forgotPassword(this.currentForm().value as UserForgotPassword)
+        .subscribe();
+      this.forgotBtnTitle = 'Reset Password';
+      this.resetPasswordMode = true;
+      this.restoreAccountMode = false;
+    }
+    if (mode === 'login' && this.currentForm().valid) {
+      const email = this.restoreAccountForm.value.email || '';
+      this.loginService
+        .resetPassword({
+          ...(this.currentForm().value as UserResetPassword),
+          email,
+        })
+        .subscribe();
+      this.forgotBtnTitle = 'Forget Password';
+      this.resetPasswordMode = false;
+      this.restoreAccountMode = false;
+    }
+    if (mode === 'cancel') {
+      this.forgotBtnTitle = 'Forget Password';
+      this.resetPasswordMode = false;
+      this.restoreAccountMode = false;
+    }
+  }
+
+  currentForm(): any {
+    if (this.restoreAccountMode) {
+      return this.restoreAccountForm;
+    } else if (this.resetPasswordMode) {
+      return this.resetPasswordForm;
+    } else {
+      return this.loginForm;
+    }
   }
 }
